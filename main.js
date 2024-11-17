b4bf97a7149b6157302b0975fed13ef72a14a8fb
Wert-bity77 <kabulovt707@gmail.com>
2024-11-17 22:35:52 +0500

    index main.js
    nma gaplola

---
+++ main.js
@@ -0,0 +1,13 @@
+let savolBiri = prompt ('String typedagi malimot kiriting')
+let savoli2 = prompt ('Number typedagi malimot kiriting')
+let savoli3 = prompt ('boolean typedagi malimot kiriting')
+
+ console.log(typeof (savolBiri));
+ console.log(typeof (Number (savoli2)))
+ console.log(typeof (Boolean (savoli3)))
+
+alert ('Hamma malumotlarni togri kiritganingizga ishonchingiz komil ')
+alert ('Siz kiritgan  ' + savolBiri+ ' soz   string typega tegishli   '   +  '\n' +'Siz kiritgan ' + savoli2+ '  sozi Number typega tegishli '     +    '\n'+ 'Siz kiritgan ' + savoli3+ '   sozi boolean typega tegishli   '    )
+ 
+
+ 
